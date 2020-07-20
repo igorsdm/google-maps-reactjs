b46834e8444234e0230a66e23113a088e0a0144d
Igor Moraes <igor@portalqualis.com.br>
2020-07-20 17:29:05 -0300

    add new marker test adjustments

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,36 +1,40 @@
 import React from 'react'
+import { act } from 'react-dom/test-utils'
 import { useSelector, useDispatch } from 'react-redux'
 import { render, fireEvent } from '~/test.utils'
 import Home from '~/pages/Home'
 
-jest.mock('react-redux', () => ({
-  useSelector: jest.fn(),
-  useDispatch: jest.fn()
-}))
+jest.mock('react-redux')
 
-const mockUseSelector = useSelector
-const mockUseDispatch = useDispatch
-const mockDispatch = jest.fn()
+const dispatch = jest.fn()
 
 describe('Google Maps Marker', () => {
-  it('should be able to add a new marker', () => {
-    mockUseSelector.mockImplementation(cb => cb({ maps: [] }))
-    mockUseDispatch.mockImplementation(() => mockDispatch)
+  it('should be able to add a new marker', async () => {
+    useDispatch.mockReturnValue(dispatch)
+    useSelector.mockImplementation(cb =>
+      cb({
+        maps: { totalMarkers: [] }
+      })
+    )
 
     const { getByTestId, getByLabelText } = render(<Home />)
 
     fireEvent.change(getByLabelText('Título'), {
       target: { value: 'Porto Alegre' }
     })
+
     fireEvent.change(getByLabelText('Latitude'), {
       target: { value: -30.034647 }
     })
+
     fireEvent.change(getByLabelText('Longitude'), {
       target: { value: -51.217659 }
     })
 
-    fireEvent.submit(getByTestId('marker-form'))
+    await act(async () => {
+      fireEvent.submit(getByTestId('marker-form'))
+    })
 
-    expect(mockUseDispatch).toHaveBeenCalled()
+    expect(dispatch).toHaveBeenCalled()
   })
 })
